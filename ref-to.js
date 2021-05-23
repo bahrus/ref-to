84b79f2cab6bb40e7e7728219ce7bc7336c98385
bahrus <anderson.bruce.b@gmail.com>
2021-05-23 16:15:32 -0400

    ...

--- ref-to.js
+++ ref-to.js
@@ -1,19 +1,18 @@
-import {ReactiveSurface, xc, IReactor, PropAction, PropDef, PropDefMap} from 'xtal-element/lib/XtalCore.js';
-
-export class RefTo extends HTMLElement implements ReactiveSurface{
+import { xc } from 'xtal-element/lib/XtalCore.js';
+export class RefTo extends HTMLElement {
     static is = 'ref-to';
     self = this;
     propActions = propActions;
-    reactor: IReactor = new xc.Rx(this);
-    wr: any;//WeakRef<Element> | undefined;
-    placeHolderMap = new WeakMap<Element, Element>();
-    a: string | undefined;
-    get deref(){
+    reactor = new xc.Rx(this);
+    wr; //WeakRef<Element> | undefined;
+    placeHolderMap = new WeakMap();
+    a;
+    get deref() {
         const element = this.wr.deref();
-        if(!element){
+        if (!element) {
             setTimeout(() => {
                 const test = this.wr.deref();
-                if(!test){
+                if (!test) {
                     this.remove();
                 }
                 return;
@@ -21,43 +20,45 @@ export class RefTo extends HTMLElement implements ReactiveSurface{
         }
         return element;
     }
-    connectedCallback(){
+    connectedCallback() {
         xc.mergeProps(this, slicedPropDefs);
     }
-    disconnectedCallback(){
+    disconnectedCallback() {
         this.deref?.remove();
     }
-    onPropChange(n: string, prop: PropDef, nv: any){
+    onPropChange(n, prop, nv) {
         this.reactor.addToQueue(prop, nv);
     }
 }
-const onA = ({a, self}: RefTo) => {
+const onA = ({ a, self }) => {
     const newElement = document.createElement(a);
-    self.wr = new WeakRef<Element>(newElement);
+    self.wr = new WeakRef(newElement);
     const childNodes = Array.from(self.childNodes);
-    for(const node of childNodes){
-        if(node instanceof RefTo){
-            if(!node.deref){
+    for (const node of childNodes) {
+        if (node instanceof RefTo) {
+            if (!node.deref) {
                 const ph = document.createElement('place--holder');
                 self.placeHolderMap.set(newElement, ph);
                 node.addEventListener('element-created', e => {
-                    const createdElement = (<any>e).detail.createdElement;
-                    if(self.placeHolderMap.has(createdElement)){
+                    const createdElement = e.detail.createdElement;
+                    if (self.placeHolderMap.has(createdElement)) {
                         const ph = self.placeHolderMap.get(createdElement);
-                        ph!.insertAdjacentElement('afterend', createdElement);
-                        ph!.remove();
-                    }else{
+                        ph.insertAdjacentElement('afterend', createdElement);
+                        ph.remove();
+                    }
+                    else {
                         throw 'NotImplementedYet'; // extra dynamic elements?
                     }
-                }, {once: true});
+                }, { once: true });
                 newElement.appendChild(ph);
-            }else{
+            }
+            else {
                 newElement.appendChild(node.deref);
             }
-        }else{
+        }
+        else {
             newElement.appendChild(node.cloneNode(true));
         }
-        
     }
     //TODO:  add mutation observer for additional ref-to direct children.
     self.dispatchEvent(new CustomEvent('element-created', {
@@ -66,11 +67,10 @@ const onA = ({a, self}: RefTo) => {
         }
     }));
 };
-
 const propActions = [
     onA
-] as PropAction[];
-const propDefMap: PropDefMap<RefTo> = {
+];
+const propDefMap = {
     a: {
         type: String,
         stopReactionsIfFalsy: true,
